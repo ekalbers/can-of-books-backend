494461f3cc1b62fd44fcafb2fe62a57c335b03da
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-02-28 14:26:29 -0800

    implements POST and DELETE methods

--- routes/books.js
+++ routes/books.js
@@ -9,7 +9,7 @@ router.get('/books', async (request, response) => {
         const book = await Book.find();
         response.status(200).json(book);
     } catch (error) {
-        response.status(400).json("there was an error");
+        response.status(500).json("server side error");
     }
 
 });
@@ -23,12 +23,20 @@ router.post('/books', async (request, response) => {
         const book = await Book.create({ title: title, author: author, description: description, status: status });
         response.status(200).json(book);
     } catch (error) {
-        response.status(400).json("there was an error post");
+        response.status(500).json("there was an error post");
     }
 })
 
-/* router.delete('/:id', async (request, response) {
+router.delete('/books/:id', async (request, response) => {
+    const { id } = request.params;
 
-}) */
+    try {
+        await Book.findByIdAndDelete(id);
+        response.status(204).send(`successfully deleted ${id}`);
+    } catch (error) {
+        response.status(404).send(`unable to delete book with id ${id}`);
+    }
+})
+
 
 module.exports = router;
